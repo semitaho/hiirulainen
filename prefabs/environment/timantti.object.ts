45338c04919bd716f9ffa876e4ba7dc244da7f0b
Toni Aho <toni@tahoo.fi>
2023-01-29 00:48:52 +0200

    muutokset

--- prefabs/environment/timantti.object.ts
+++ prefabs/environment/timantti.object.ts
@@ -7,7 +7,7 @@ export class TimanttiObject extends EnvironmentObject {
   constructor(scene: BABYLON.Scene, index: number) {
     super("timantti"+ index);
     const material = new BABYLON.StandardMaterial("paita"+index);
-    material.diffuseTexture = new BABYLON.Texture("./../textures/pear/food_pears_asian_01_diff_4k.jpg");
+    material.diffuseTexture = new BABYLON.Texture("./textures/pear/food_pears_asian_01_diff_4k.jpg");
     this.mesh.material = material;
    
 

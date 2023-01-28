4189b5d5994f9fa37d1d333fea9f66e645446695
Toni Aho <toni@tahoo.fi>
2023-01-28 20:57:34 +0200

    files

--- webpack.config.js
+++ webpack.config.js
@@ -2,46 +2,52 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 const webpack = require("webpack");
 
-module.exports = {
-    entry: './index.ts',
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-
-    devServer: {
-        static: {
-            directory: __dirname,
+const RemoveConsolePlugin = require('remove-console-webpack-plugin');
+module.exports = (env) => {
+    console.log ("env", env);
+    return {
+        entry: './index.ts',
+        output: {
+            filename: 'index.js',
+            path: path.resolve(__dirname, 'dist')
         },
-        compress: true,
-        port: 9000,
-        open: true,
-        hot: true
-    },
-    resolve: {
-        extensions: [".ts", ".js"]
-    },
-    module: {
-        rules: [
-            { test: /\.tsx?$/, loader: "ts-loader" },
-            {
-                test: /\babylon.dynamicTerrain.js\.js$/,
-                use: ['imports-loader?BABYLON=>require("babylonjs")']
-            }
-        ]
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            "CANNON": "cannon"
-        }),
-        new webpack.ProvidePlugin({
-            'BABYLON': 'babylonjs',
-        }),
-        new HtmlWebpackPlugin({
-            inject: true,
-            template: path.resolve('./', "index.html"),
-        }),
-    ],
-    mode: "development"
+
+
+        devServer: {
+            static: {
+                directory: __dirname,
+            },
+            compress: true,
+            port: 9000,
+            open: true,
+            hot: true
+        },
+        resolve: {
+            extensions: [".ts", ".js"]
+        },
+        module: {
+            rules: [
+                { test: /\.tsx?$/, loader: "ts-loader" },
+                {
+                    test: /\babylon.dynamicTerrain.js\.js$/,
+                    use: ['imports-loader?BABYLON=>require("babylonjs")']
+                }
+            ]
+        },
+        plugins: [
+            new webpack.ProvidePlugin({
+                "CANNON": "cannon"
+            }),
+
+            new webpack.ProvidePlugin({
+                'BABYLON': 'babylonjs',
+            }),
+            new HtmlWebpackPlugin({
+                inject: true,
+                template: path.resolve('./', "index.html"),
+            }),
+        ],
+        mode: "development"
+    };
 
 };

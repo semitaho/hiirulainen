8bbe84cfa98c00263241843fb185bd446e70c644
semitaho <toni@tahoo.fi>
2023-02-02 18:26:51 +0000

    Deploying to gh-pages from @ semitaho/hiirulainen@a86a94e0a5613b8071c339b8d117003fab46c98e 🚀

--- audio/index.ts
+++ audio/index.ts
@@ -1,62 +1,66 @@
 import { ISoundOptions, Scene } from "babylonjs";
 import { HiirulainenAudio } from "./hiirulainen.audio";
+import { AudiosModel } from './../models/audios.model';
 
 
+export function loadAudio(scene: Scene): AudiosModel {
+  createHiirulainenAudio("tunetank.mp3", scene, {
+    volume: 0.2, autoplay: false, loop: true
+  }).then((audio: HiirulainenAudio) => audio.play());
 
-export function loadAudio(scene: Scene): void {
-    createHiirulainenAudio("tunetank.mp3", scene, {
-        volume: 0.2, autoplay: false, loop: true
-    }).then((audio: HiirulainenAudio) => audio.play());
+  startLaskeminen(scene);
 
-    startLaskeminen(scene);
+  return {
+    loytyi: createHiirulainenAudio("loytyi.m4a", scene, {volume: 1.2,  }) 
+    };
 }
 function createHiirulainenAudio(filenameWithoutPath: string, scene: Scene, options: ISoundOptions): Promise<HiirulainenAudio> {
-    return new Promise((resolve, _) => {
-        const hiirulainenAudio = new HiirulainenAudio(filenameWithoutPath, scene, () => {
-            resolve(hiirulainenAudio);
-        }, options);
-    });
+  return new Promise((resolve, _) => {
+    const hiirulainenAudio = new HiirulainenAudio(filenameWithoutPath, scene, () => {
+      resolve(hiirulainenAudio);
+    }, options);
+  });
 }
 
 
 function afterAudioPlayedCreateNewPlayeable(audio: HiirulainenAudio, newAudioPromise: Promise<HiirulainenAudio>): Promise<HiirulainenAudio> {
-    return new Promise(resolve => {
-        audio.onEndedObservable.add(() => {
-            resolve(newAudioPromise);
-        });
-
+  return new Promise(resolve => {
+    audio.onEndedObservable.add(() => {
+      resolve(newAudioPromise);
     });
+
+  });
 
 }
 
 function startLaskeminen(scene: Scene) {
-    createHiirulainenAudio("Yksi.m4a", scene, {
-        loop: false,
-        volume: 0.6
-    }).then((audio: HiirulainenAudio) => {
-        audio.play(2);
-        return afterAudioPlayedCreateNewPlayeable(
-            audio,
-            createHiirulainenAudio("Kaksi.m4a", scene, null));
-    }).then((newAudio: HiirulainenAudio) => {
-        newAudio.play(1);
-        return afterAudioPlayedCreateNewPlayeable(newAudio,
-            createHiirulainenAudio("Kolme.m4a", scene, null));
-    }).then((newAudio: HiirulainenAudio) => {
-        newAudio.play(1);
-        return afterAudioPlayedCreateNewPlayeable(newAudio,
-            createHiirulainenAudio("Nelja.m4a", scene, null));
-    }).then((newAudio: HiirulainenAudio) => {
-        newAudio.play(1);
-        return afterAudioPlayedCreateNewPlayeable(newAudio,
-            createHiirulainenAudio("Viisi.m4a", scene, {
-                volume: 0.4
-            }));
-    }).then((newAudio: HiirulainenAudio) => {
-        newAudio.play(1);
-        return afterAudioPlayedCreateNewPlayeable(newAudio,
-            createHiirulainenAudio("Tullaan.m4a", scene, null));
-    }).then((newAudio: HiirulainenAudio) => {
-        newAudio.play(2);
-    });
+  createHiirulainenAudio("Yksi.m4a", scene, {
+    loop: false,
+    volume: 0.6
+  }).then((audio: HiirulainenAudio) => {
+    audio.play(2);
+    return afterAudioPlayedCreateNewPlayeable(
+      audio,
+      createHiirulainenAudio("Kaksi.m4a", scene, null));
+  }).then((newAudio: HiirulainenAudio) => {
+    newAudio.play(1);
+    return afterAudioPlayedCreateNewPlayeable(newAudio,
+      createHiirulainenAudio("Kolme.m4a", scene, null));
+  }).then((newAudio: HiirulainenAudio) => {
+    newAudio.play(1);
+    return afterAudioPlayedCreateNewPlayeable(newAudio,
+      createHiirulainenAudio("Nelja.m4a", scene, null));
+  }).then((newAudio: HiirulainenAudio) => {
+    newAudio.play(1);
+    return afterAudioPlayedCreateNewPlayeable(newAudio,
+      createHiirulainenAudio("Viisi.m4a", scene, {
+        volume: 0.4
+      }));
+  }).then((newAudio: HiirulainenAudio) => {
+    newAudio.play(1);
+    return afterAudioPlayedCreateNewPlayeable(newAudio,
+      createHiirulainenAudio("Tullaan.m4a", scene, null));
+  }).then((newAudio: HiirulainenAudio) => {
+    newAudio.play(2);
+  });
 }

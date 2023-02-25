9f8a7a4c59514fb79b2e43203753f1c0a111ae41
Toni Aho <toni@tahoo.fi>
2023-02-25 17:11:00 +0200

    KATSO

--- sw.js
+++ sw.js
@@ -67,7 +67,7 @@ self.addEventListener('fetch', event => {
       }
 
       
-      return caches.open(RUNTIME).then(cache => {
+      return caches.open(CACHE_NAME).then(cache => {
         return fetch(event.request).then(response => {
           // Put a copy of the response in the runtime cache.
           return cache.put(event.request, response.clone()).then(() => {
